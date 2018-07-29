d44e5ac13c17c147bae70744cdca3cff1a9f0e78
Venkatesh1505 <venkyravi97@gmail.com>
2018-07-29 17:12:20 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
     $('#ajaxloading').show();
     $('#reload').show();
   });
-  $('#title').text("Dynamic Quiz");
+  $('#title').text("Quiz on AWS");
   $('#title').hide().fadeIn("slow");
   $('#nameForm').hide().fadeIn("slow");
   $('#description').hide();
